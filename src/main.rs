658b8393804dee20a3e895b788ceb6c1ce47f418
bpoetz <bryan@bpoetz.ca>
2019-12-05 00:44:47 -0500

    refactor to remove some fluff

--- src/main.rs
+++ src/main.rs
@@ -5,19 +5,22 @@ use std::collections::HashMap;
 // from left to right
 // lb 246515 739105
 //
-type Password = [i32;6];
-
-fn check_password (pass: Password)-> bool {
+type Password = [i32;6];//this is just an alias, we can't add methods to this as in a struct
+// could do struct Password ([i32;6]) or struct Password(i32, i32, i32, i32, i32, i32)
+fn check_password (password: Password)-> bool {
     let mut biggest = &pass[0];
     let mut consecutive_digits:HashMap<i32, i32> = HashMap::new();
-    let mut has_consecutive: bool = false;
 
-    for (i,x) in pass.iter().enumerate(){
+    for (i,x) 
+    in password
+        .iter()
+        .enumerate(){
         if i>0 && x==&pass[i-1]{
-            let count = consecutive_digits.entry(*x).or_insert(0);
-            *count +=1;
-
-            has_consecutive = true;
+            //consecutive digit found.
+            let count = consecutive_digits
+                .entry(*x) //return the hash map value for key x
+                .or_insert(0); // or initialize the key to 0
+            *count +=1; // either way, we increment it.
         } 
 
         if x>=biggest {
@@ -26,43 +29,32 @@ fn check_password (pass: Password)-> bool {
         else{
             return false;
         }
-
     }
-    let at_least_one_unique: bool = consecutive_digits
-        .values()
-        .map(|&x| x == 1__i32)
-        .fold(false, |result, x| result || x);
-
-    has_consecutive && at_least_one_unique
-}
-
-fn password_to_number(pass: Password) -> i32{
-    pass
-        .iter()
-        .enumerate()
-        .map(|(i,x)| x * 10_i32.pow((5 - i) as u32))
-        .sum()
+    consecutive_digits // for each consecutive hash map entry
+        .values() // pick just the value (don't care which digit it is)
+        .map(|&x| x == 1__i32) // check if that value was unique
+        .fold(false, |result, x| result || x) // check if any of those comparisons were true
 }
 
 fn number_to_password(num:i32) -> Password {
-    let mut pass = [0,0,0,0,0,0];
-    //TODO: refactor as a vector?
+    let mut password = [0,0,0,0,0,0];
+    // magic #s are good, actually
     for i in 1..7{
-        pass[6-i] = (num % 10_i32.pow(i as u32) ) / 10_i32.pow((i-1) as u32);
+        password[6-i] = (num % 10_i32.pow(i as u32) ) / 10_i32.pow((i-1) as u32);
     }
-    pass
+    password
 }
 fn main(){
-    let lb: Password = [2,4,6,5,1,5];
+    let lb: i32 = 246515;
     let ub: i32 = 739105;
     let mut c: i32 = 0;
-    let mut pass: Password = lb;
+    let mut password: i32 = lb;
 
-    while password_to_number(pass) < ub {
-        if check_password(pass){
+    while password < ub {
+        if check_password(number_to_password(password)){
             c = c + 1;
         };
-        pass = number_to_password(password_to_number(pass) + 1);
+        password = password + 1;
     }
     println!("{:?}",c);
 
