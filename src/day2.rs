0c01a822047dad73293ed4674f2d4ebaa0d24d45
bpoetz <bryan@bpoetz.ca>
2019-12-05 00:15:41 -0500

    skipped day 3 - day 4 done

---
+++ src/day2.rs
@@ -0,0 +1,29 @@
+fn create_intcode(noun:usize, verb: usize) -> [usize;121]{
+    [1,noun, verb,3,1,1,2,3,1,3,4,3,1,5,0,3,2,10,1,19,2,19,6,23,2,13,23,27,1,9,27,31,2,31,9,35,1,6,35,39,2,10,39,43,1,5,43,47,1,5,47,51,2,51,6,55,2,10,55,59,1,59,9,63,2,13,63,67,1,10,67,71,1,71,5,75,1,75,6,79,1,10,79,83,1,5,83,87,1,5,87,91,2,91,6,95,2,6,95,99,2,10,99,103,1,103,5,107,1,2,107,111,1,6,111,0,99,2,14,0,0]
+
+}
+
+
+fn main(){
+
+for x in 0..100{
+    for y in 0..100{
+        let mut intcode = create_intcode(x, y);
+        for i in 0..121{ 
+            if i % 4 == 0{
+                let val = match intcode[i] {
+                1 => intcode[intcode[i+1]] + intcode[intcode[i+2]],
+                2 => intcode[intcode[i+1]] * intcode[intcode[i+2]],
+                99 => break,
+                _ => panic!("{}, intcode[i]")
+                };
+                intcode[intcode[i+3]] = val;
+
+            }
+        }
+        if intcode[0] == 19690720{
+            panic!("{}", x*100 + y);
+        }
+    }
+}
+}
--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,69 @@
-fn create_intcode(noun:usize, verb: usize) -> [usize;121]{
-    [1,noun, verb,3,1,1,2,3,1,3,4,3,1,5,0,3,2,10,1,19,2,19,6,23,2,13,23,27,1,9,27,31,2,31,9,35,1,6,35,39,2,10,39,43,1,5,43,47,1,5,47,51,2,51,6,55,2,10,55,59,1,59,9,63,2,13,63,67,1,10,67,71,1,71,5,75,1,75,6,79,1,10,79,83,1,5,83,87,1,5,87,91,2,91,6,95,2,6,95,99,2,10,99,103,1,103,5,107,1,2,107,111,1,6,111,0,99,2,14,0,0]
+use std::collections::HashMap;
+// rules:
+// 6 digits
+// at least two adjacent #'s are the same
+// from left to right
+// lb 246515 739105
+//
+type Password = [i32;6];
 
-}
-
-
-fn main(){
-
-for x in 0..100{
-    for y in 0..100{
-        let mut intcode = create_intcode(x, y);
-        for i in 0..121{ 
-            if i % 4 == 0{
-                let val = match intcode[i] {
-                1 => intcode[intcode[i+1]] + intcode[intcode[i+2]],
-                2 => intcode[intcode[i+1]] * intcode[intcode[i+2]],
-                99 => break,
-                _ => panic!("{}, intcode[i]")
-                };
-                intcode[intcode[i+3]] = val;
-
-            }
+fn check_password (pass: Password)-> bool {
+    let mut biggest = &pass[0];
+    let mut consecutive_digits:HashMap<i32, i32> = HashMap::new();
+    let mut has_consecutive: bool = false;
+    
+    for (i,x) in pass.iter().enumerate(){
+        if i>0 && x==&pass[i-1]{
+            let count = consecutive_digits.entry(*x).or_insert(0);
+            *count +=1;
+            
+            has_consecutive = true;
+        } 
+        
+        if x>=biggest {
+            biggest = x;
         }
-        if intcode[0] == 19690720{
-            panic!("{}", x*100 + y);
+        else{
+            return false;
         }
+    
     }
+    let at_least_one_unique: bool = consecutive_digits
+        .values()
+        .map(|&x| x == 1__i32)
+        .fold(false, |result, x| result || x);
+    
+    has_consecutive && at_least_one_unique
 }
+
+fn password_to_number(pass: Password) -> i32{
+    pass
+      .iter()
+      .enumerate()
+      .map(|(i,x)| x * 10_i32.pow((5 - i) as u32))
+      .sum()
+}
+
+fn number_to_password(num:i32) -> Password {
+    let mut pass = [0,0,0,0,0,0];
+    for i in 1..7{
+        pass[6-i] = (num % 10_i32.pow(i as u32) ) / 10_i32.pow((i-1) as u32);
+    }
+    pass
+}
+fn main(){
+    let lb: Password = [2,4,6,5,1,5];
+    let ub: i32 = 739105;
+    let mut c: i32 = 0;
+    let mut pass: Password = lb;
+
+    println! ("{:?}", number_to_password(ub));
+    while password_to_number(pass) < ub {
+        if check_password(pass){
+            c = c + 1;
+        };
+        pass = number_to_password(password_to_number(pass) + 1);
+    }
+    println!("{:?}",c);
+    
 }
